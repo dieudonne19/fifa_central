1e9750822372f9e5fbde1e279ec0642416f31538
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-05-06 14:19:03 +0300

    feat : best clubs filters

--- src/main/java/org/football/fifa_central/service/BestClubService.java
+++ src/main/java/org/football/fifa_central/service/BestClubService.java
@@ -3,24 +3,31 @@ package org.football.fifa_central.service;
 
 import lombok.RequiredArgsConstructor;
 import org.football.fifa_central.dao.operations.ClubStatsCrudOperations;
+import org.football.fifa_central.model.Club;
 import org.football.fifa_central.model.ClubStats;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-@Service@RequiredArgsConstructor
+@Service
+@RequiredArgsConstructor
 public class BestClubService {
 
     private final ClubStatsCrudOperations clubStatsCrudOperations;
 
-    public ResponseEntity<Object> getBestClubs(int top){
+    public ResponseEntity<Object> getBestClubs(int top) {
 
         List<ClubStats> clubStatsList = clubStatsCrudOperations.getAll();
 
-        clubStatsList.stream().sorted((o1, o2) -> o1.getRankingPoints().compareTo(o2.getRankingPoints()  )).limit(top).toList();
-        clubStatsList.
-        return ResponseEntity.ok(clubStatsList);
+        List<ClubStats> sortedStats = clubStatsList.stream().sorted(Comparator.comparingInt(ClubStats::getRankingPoints).reversed()
+                .thenComparing(Comparator.comparingInt(ClubStats::getDifferenceGoals).reversed()
+                        .thenComparing(Comparator.comparingInt(ClubStats::getCleanSheetNumber).reversed())))
+                .limit(top).toList();
+
+        return ResponseEntity.ok(sortedStats);
 
     }
 }
