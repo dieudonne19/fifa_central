242b5829a64969634c7359c502e8f61bf14c275d
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-05-06 11:06:20 +0300

    feat : separated contrllers files

--- src/main/java/org/football/fifa_central/endpoint/BestPlayersController.java
+++ src/main/java/org/football/fifa_central/endpoint/BestPlayersController.java
@@ -1,24 +1,21 @@
 package org.football.fifa_central.endpoint;
 
+
 import lombok.RequiredArgsConstructor;
 import org.football.fifa_central.model.DurationUnit;
 import org.football.fifa_central.service.PlayerService;
-import org.football.fifa_central.service.SynchronizationService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class SynchronizationController {
-    private final SynchronizationService synchronizationService;
+public class BestPlayersController {
 
+    private final PlayerService playerService;
 
-    @GetMapping("/synchronisation")
-    public Object synchronize() {
-        return synchronizationService.synchronize();
+    @GetMapping("/bestPlayers")
+    public Object getBestPlayers(@RequestParam(name = "top", required = false) Integer top, @RequestParam(name = "playingTimeUnit", defaultValue = "MINUTE") DurationUnit playingTimeUnit) {
+        return playerService.getBestPlayerFromAllChampionship(top, playingTimeUnit);
     }
-
-
-
 }
